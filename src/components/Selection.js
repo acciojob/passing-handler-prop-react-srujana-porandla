c639e9e9439910a497b68e8e7027ccef2dc793c8
Srujana <p.srujana1356@gmail.com>
2024-07-07 15:59:37 +0530

    select

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,17 @@
+import React, { useState, useEffect } from 'react';
 
+const Selection = ({ selectedColor }) => {
+  const [color, setColor] = useState(selectedColor);
+
+  useEffect(() => {
+    setColor(selectedColor);
+  }, [selectedColor]);
+
+  return (
+    <div className="fix-box" style={{ backgroundColor: color }}>
+      <h2>Selection</h2>
+    </div>
+  )
+}
+
+export default Selection;
